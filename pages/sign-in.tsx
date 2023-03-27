f1eac158b0694aedd45d14f3979384efe777b546
Filip Asprovski <35760671+asprovskif@users.noreply.github.com>
2023-03-27 15:24:12 +0200

    add form validation

--- pages/sign-in.tsx
+++ pages/sign-in.tsx
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Page from '../components/Page';
 import Input from '../components/Input';
 import Field from '../components/Field';
 import Button from '../components/Button';
 
 const SignIn: React.FC<any> = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log('should submit', {email, password})
+    }
+
     return (
         <Page title="Sign in">
-            <form>
+            <form onSubmit={handleSubmit}>
                 <Field label="Email">
-                    <Input type="email"/>
+                    <Input type="email" value={email} required
+                    onChange={(event) => setEmail(event.target.value)}
+                    />
                 </Field>
                 <Field label="Password">
-                    <Input type="password"/>
+                    <Input type="password" value={password} required
+                    onChange={(event) => setPassword(event.target.value)}
+                    />
                 </Field>
                 <Button type="submit">
                     Sign in
