ed7a6360d9322e3d11f2d1f65a734cfd68573773
Filip Asprovski <35760671+asprovskif@users.noreply.github.com>
2023-03-28 10:00:58 +0200

    add Error and Loading states

--- pages/sign-in.tsx
+++ pages/sign-in.tsx
@@ -8,16 +8,22 @@ import {fetchJson} from '../lib/api';
 const SignIn: React.FC<any> = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [status, setStatus] = useState({loading: false, error: false});
 
     const handleSubmit = async (event) => {
         event.preventDefault();
-        const response = await fetchJson('http://localhost:1337/auth/local', {
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({identifier: email, password}),
-        })
-
-        console.log('response', response);
+        setStatus({loading: true, error: false});
+        try {
+            const response = await fetchJson('http://localhost:1337/auth/local', {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({identifier: email, password}),
+            })
+            setStatus({loading: false, error: false});
+            console.log('response', response);
+        } catch (err) {
+            setStatus({loading: false, error: true});
+        }
     }
 
     return (
@@ -25,17 +31,26 @@ const SignIn: React.FC<any> = () => {
             <form onSubmit={handleSubmit}>
                 <Field label="Email">
                     <Input type="email" value={email} required
-                    onChange={(event) => setEmail(event.target.value)}
+                           onChange={(event) => setEmail(event.target.value)}
                     />
                 </Field>
                 <Field label="Password">
                     <Input type="password" value={password} required
-                    onChange={(event) => setPassword(event.target.value)}
+                           onChange={(event) => setPassword(event.target.value)}
                     />
                 </Field>
-                <Button type="submit">
-                    Sign in
-                </Button>
+                {status.error && (
+                    <p className='text-red-700'>
+                        Invalid credentials
+                    </p>
+                )}
+                {status.loading ? (
+                    <p>Loading...</p>
+                ) : (
+                    <Button type="submit">
+                        Sign in
+                    </Button>
+                )}
             </form>
         </Page>
     );
